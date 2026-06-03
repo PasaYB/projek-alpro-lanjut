d0af209c793113a7900e02e483eade2d52709b35
PasaaYB <lananggading.pasa@gmail.com>
2026-06-03 20:16:47 +0700

    fix: minor fix

--- main.cpp
+++ main.cpp
@@ -13,20 +13,20 @@ void menuAdmin(){
     do
     {
         titleScreen("DASHBOARD ADMIN");
-        std::cout << "1. Tambah Daftar Penerbangan" << endl
+        cout << "1. Tambah Daftar Penerbangan" << endl
             << "2. Cari Daftar Penerbangan" << endl
             << "3. Lihat Daftar Penerbangan" << endl
             << "4. Edit Daftar Penerbangan" << endl
             << "5. Hapus Daftar Penerbangan" << endl
             << "6. Keluar" << endl
-            << "Pilih menu: "; std::cin >> pilihan;
+            << "Pilih menu: "; cin >> pilihan;
 
             if(pilihan == 1){ tambahJadwal(j); }
             else if(pilihan == 2){ cariJadwal(j); }
             else if(pilihan == 3){ lihatJadwal(j); }
             else if(pilihan == 4){ editJadwal(j); }
             else if(pilihan == 5){ hapusJadwal(j); }
-            else if(pilihan == 6){ std::cout << "Terima gaji" << endl;}
+            else if(pilihan == 6){ cout << "Terima gaji" << endl;}
     } while (pilihan != 6);
 }
 // ================================================================================================
@@ -37,17 +37,17 @@ void mainMenu(){
     do
     {
         titleScreen("Menu Sistem Reservasi User");
-        std::cout << "1. Lihat Daftar Penerbangan" << endl
+        cout << "1. Lihat Daftar Penerbangan" << endl
             << "2. Pesan Tiket" << endl
             << "3. Kelola Pesanan" << endl
             << "4. Laporan" << endl
             << "5. Keluar" << endl
-            << "Pilih menu: "; std::cin >> pilihan;
+            << "Pilih menu: "; cin >> pilihan;
 
             if(pilihan == 1){ lihatDaftarPenerbangan(); }
             else if(pilihan == 2){ pesanTiket(j); }
             else if(pilihan == 3){ laporanPesanan(); }
-            else if(pilihan == 4){ std::cout << "Keluar dari menu user.\n"; }
+            else if(pilihan == 4){ cout << "Keluar dari menu user.\n"; }
     } while (pilihan != 4);
 }
 // ================================================================================================
@@ -59,16 +59,16 @@ void login(){
     do
     {
         titleScreen("LOGIN");
-        std::cout << "1. Login sebagai Admin" << endl
+        cout << "1. Login sebagai Admin" << endl
             << "2. Login sebagai User" << endl
             << "3. Keluar" << endl
-            << "Pilih menu: "; std::cin >> pilihan;
+            << "Pilih menu: "; cin >> pilihan;
             
         if(pilihan == 1){
             do {
                 titleScreen("LOGIN ADMIN");
-                std::cout << "Masukkan nama Admin: "; std::cin >> admin_name;
-                std::cout << "Masukkan password: "; std::cin >> admin_pw;
+                cout << "Masukkan nama Admin: "; cin >> admin_name;
+                cout << "Masukkan password: "; cin >> admin_pw;
 
                 stoi(admin_pw);
                 
@@ -79,13 +79,13 @@ void login(){
                     chance++;
                 }
 
-                std::cout << "Kesempatan Habis" << endl;
+                cout << "Kesempatan Habis" << endl;
             } while(chance < 3);
         } else if (pilihan == 2) {
             do {
                 titleScreen("LOGIN USER");
-                std::cout << "Masukkan nama User: "; std::cin >> user_name;
-                std::cout << "Masukkan password: "; std::cin >> user_pw;
+                cout << "Masukkan nama User: "; cin >> user_name;
+                cout << "Masukkan password: "; cin >> user_pw;
 
                 if(user_name == "user" && user_pw == "123"){
                     mainMenu();
@@ -94,13 +94,13 @@ void login(){
                     chance++;
                 }
 
-                std::cout << "Kesempatan Habis" << endl;
+                cout << "Kesempatan Habis" << endl;
             } while (chance < 3);
             
         }
     } while (pilihan != 3);
 
-    std::cout << "Terima Kasih!" << endl;
+    cout << "Terima Kasih!" << endl;
 }
 
 int main() {
